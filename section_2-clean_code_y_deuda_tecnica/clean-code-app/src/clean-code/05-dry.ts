4607a72c8d8cab2dbd03a2d3dfa8e1163269fa12
Adriano Ayala <adriano.ayc@gmail.com>
2025-04-15 12:33:36 -0400

    Aplicando DRY

--- section_2-clean_code_y_deuda_tecnica/clean-code-app/src/clean-code/05-dry.ts
+++ section_2-clean_code_y_deuda_tecnica/clean-code-app/src/clean-code/05-dry.ts
@@ -7,11 +7,34 @@ class Product {
     public size: Size = ''
   ) {}
 
+  isProductReady(): boolean {
+    // Applying DRY
+    for (const key in this) {
+      //   console.log(key, typeof this[key]);
+      switch (typeof this[key]) {
+        case 'string':
+          //   if ((this[key] + '').length <= 0) throw new Error(`${key} is empty`);
+          if ((<string>this[key]).length <= 0)
+            throw new Error(`${key} is empty`);
+          break;
+        case 'number':
+          if (<number>this[key] <= 0) throw new Error(`${key} is zero'`);
+          break;
+        default:
+          throw new Error(`${typeof this[key]} is not valid`);
+      }
+    }
+
+    return true;
+  }
+
   toString() {
     // No DRY
-    if (this.name.length <= 0) throw new Error('name is empty');
-    if (this.price <= 0) throw new Error('price is zero');
-    if (this.size.length <= 0) throw new Error('size is empty');
+    // if (this.name.length <= 0) throw new Error('name is empty');
+    // if (this.price <= 0) throw new Error('price is zero');
+    // if (this.size.length <= 0) throw new Error('size is empty');
+
+    if (!this.isProductReady()) return;
 
     return `${this.name} (${this.price}), ${this.size}`;
   }
