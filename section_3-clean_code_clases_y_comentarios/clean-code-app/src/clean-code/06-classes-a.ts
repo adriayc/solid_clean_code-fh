96cd239ac29ae12fb39fb70331ac89e7558857b0
Adriano Ayala <adriano.ayc@gmail.com>
2025-04-16 14:30:28 -0400

    Herencia - Problemática

--- section_3-clean_code_clases_y_comentarios/clean-code-app/src/clean-code/06-classes-a.ts
+++ section_3-clean_code_clases_y_comentarios/clean-code-app/src/clean-code/06-classes-a.ts
@@ -1,4 +1,6 @@
 (() => {
+  // No aplicando el principio de respoonsabilidad única
+
   type Gender = 'M' | 'F';
 
   // Long form
@@ -23,6 +25,53 @@
     ) {}
   }
 
-  const newPerson = new Person('Adriano', 'M', new Date('1990-02-20'));
-  console.log(newPerson);
+  class User extends Person {
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+    }
+  }
+
+  //   const newPerson = new Person('Adriano', 'M', new Date('1990-02-20'));
+  //   console.log(newPerson);
+
+  const newUserSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'adriano@mail.com',
+    'Admin',
+    'Adriano',
+    'M',
+    new Date('1990-02-20')
+  );
+  console.log({
+    newUserSettings,
+    // areCredentialsValid: newUserSettings.checkCredentials(),
+  });
 })();
