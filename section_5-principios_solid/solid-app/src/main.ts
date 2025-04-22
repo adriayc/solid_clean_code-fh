799a9d832d1c67bf6ab451e26101923c5c74ae3e
Adriano Ayala <adriano.ayc@gmail.com>
2025-04-22 11:05:28 -0400

    Introducción

---
+++ section_5-principios_solid/solid-app/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Clean Code y SOLID</h1>
+    <span>Revisar la consola de JavaScript</span>
+  </div>
+`;
