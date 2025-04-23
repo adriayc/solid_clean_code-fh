e9560b9965b69fa5a2ef6fa9a70a26b4d483bea8
Adriano Ayala <adriano.ayc@gmail.com>
2025-04-23 18:15:00 -0400

    Solución aplicando los principios OCP y LSP

--- section_5-principios_solid/solid-app/src/main.ts
+++ section_5-principios_solid/solid-app/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 // import './solid/01-srp-final';
 // import './solid/02-ocp-final-a';
-import './solid/03-lsp-start-a';
+import './solid/03-lsp-final-a';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
